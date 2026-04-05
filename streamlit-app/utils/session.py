b0a3f4eb1efb2179e53451d7b58b6e3c5b95cd47
hwanghyeonseo <hhseo1202@naver.com>
2026-04-05 11:10:00 +0900

    1

--- streamlit-app/utils/session.py
+++ streamlit-app/utils/session.py
@@ -14,6 +14,7 @@ def init_state():
         "keyword_page_info": None,
         "article_page_info": None,
         "importance_page_info": None,
+        "is_logged_in": False,
     }
 
     for key, value in defaults.items():
@@ -25,6 +26,14 @@ def clear_auth_state():
     st.session_state["access_token"] = None
     st.session_state["refresh_token"] = None
     st.session_state["user"] = None
+    st.session_state["is_logged_in"] = False
+
+
+def set_auth_state(access_token: str, refresh_token: str | None = None, user=None):
+    st.session_state["access_token"] = access_token
+    st.session_state["refresh_token"] = refresh_token
+    st.session_state["user"] = user
+    st.session_state["is_logged_in"] = True
 
 
 def set_selected_keyword(keyword_id, keyword_name):
